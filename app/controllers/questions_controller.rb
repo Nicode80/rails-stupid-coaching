9163d29472f750a620b5707e6f94c22ba7e9dee9
Nicolas Constantin <nicolasconstantin@hotmail.fr>
2020-11-09 18:45:03 +0100

    tests added to stupid coach

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,13 @@ class QuestionsController < ApplicationController
 
   end
   def answer
-    @answer = params[:answer]
+    if params[:question] == 'I am going to work'
+      @answer = 'Great!'
+    elsif params[:question].chars.last == '?'
+      @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I don\'t care, get dressed and go to work!'
+    end
+
   end
 end
